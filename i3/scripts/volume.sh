739d1cd9e16dc3178babd7e2952256281ff552f4
Dipankar Das <dipankardas0115@gmail.com>
2023-03-31 19:20:11 +0530

    Removed the junk in i3 config
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- i3/scripts/volume.sh
+++ i3/scripts/volume.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-max_volume_pc=$1
+operation=$1
+
+max_volume_pc=100
+max_volume_pc_upper_up=90
+
+
 curr_volume=$(pactl get-sink-volume $(pactl get-default-sink)  | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' | head -n1)
 
+if [[ $operation == "UP" ]]; then
 
-if [ $curr_volume -le 90 ]; then 
-    pactl set-sink-volume @DEFAULT_SINK@ +10% && $refresh_i3status
+    if [[ $curr_volume -le $max_volume_pc_upper_up ]]; then 
+        pactl set-sink-volume @DEFAULT_SINK@ +10% && notify-send "Sound level: $(pactl get-sink-volume $(pactl get-default-sink)  | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' | head -n1)" && $refresh_i3status
+
+    else
+        diff=$(( $max_volume_pc - $curr_volume ))
+        pactl set-sink-volume @DEFAULT_SINK@ +$diff% && notify-send "Sound level: $(pactl get-sink-volume $(pactl get-default-sink)  | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' | head -n1)" && $refresh_i3status
+    fi
+
+elif [[ $operation == "DOWN" ]]; then
+    pactl set-sink-volume @DEFAULT_SINK@ -10% && notify-send "Sound level: $(pactl get-sink-volume $(pactl get-default-sink)  | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,' | head -n1)" && $refresh_i3status
+
 else
-    diff=$(( $max_volume_pc - $curr_volume ))
-    pactl set-sink-volume @DEFAULT_SINK@ +$diff% && $refresh_i3status
+    notify-send "INVALID OPERATION on sound"
 fi
