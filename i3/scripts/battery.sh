1658707f8bb297859fc31ff824f0f2f779ee12bb
Dipankar Das <dipankardas0115@gmail.com>
2023-05-26 16:07:38 +0530

    updated the configs
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- i3/scripts/battery.sh
+++ i3/scripts/battery.sh
@@ -4,24 +4,26 @@ currBat=$(cat /sys/class/power_supply/BAT0/capacity)
 
 stats=$(cat /sys/class/power_supply/BAT0/status)
 
+tt=$(date +'%H:%M %d-%m-%y')
 
+echo "--------"
 if [[ $currBat -lt 80 && $stats = 'Discharging' ]]; then
     # normal mode
-    echo "start charging"
+    echo "${tt} start charging ${currBat}"
     sudo sed -i s/STOP_CHARGE_THRESH_BAT0=1/STOP_CHARGE_THRESH_BAT0=0/g /etc/tlp.conf
     sudo tlp start
 elif [[ $currBat -lt 80 && $stats = 'Not charging' ]]; then
     # normal mode
-    echo "start charging"
+    echo "${tt} start charging ${currBat}"
     sudo sed -i s/STOP_CHARGE_THRESH_BAT0=1/STOP_CHARGE_THRESH_BAT0=0/g /etc/tlp.conf
     sudo tlp start
 elif [[ $currBat -gt 85 && $stats = 'Charging' ]]; then
     # conservative mode
-    echo "stop charging"
+    echo "${tt} stop charging ${currBat}"
     sudo sed -i s/STOP_CHARGE_THRESH_BAT0=0/STOP_CHARGE_THRESH_BAT0=1/g /etc/tlp.conf
     sudo tlp start
 else
-    echo "No action needed"
+    echo "${tt} No action needed ${currBat}"
 fi
 
 echo "--------"
