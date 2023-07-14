776fa4b8482a0a579add3dc9f2bf37ae9d6ca90f
Dipankar Das <dipankardas0115@gmail.com>
2023-07-14 23:37:45 +0530

    Added the configs
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -144,7 +144,12 @@ require('lazy').setup({
 
   'mfussenegger/nvim-dap',
 
-  "rcarriga/nvim-dap-ui",
+  {
+    "rcarriga/nvim-dap-ui",
+    config = function()
+      require("dapui").setup({})
+    end,
+  },
 
   -- Git related plugins
   'tpope/vim-fugitive',
@@ -341,6 +346,36 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+
+
+
+
+vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
+vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
+vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
+vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>dU', function() require('dapui').toggle() end)
+vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+vim.keymap.set({'n', 'v'}, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require('dap.ui.widgets')
+  widgets.centered_float(widgets.scopes)
+end)
+
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
