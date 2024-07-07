e7659a0553de7fc08c4a59200bd98f2f2b5c6a60
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-07-07 12:41:13 +0530

    updated the changes
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- nvim/init-new.lua
+++ nvim/init-new.lua
@@ -40,7 +40,83 @@ require('lazy').setup({
     end,
 
   },
-
+  {
+    "tpope/vim-dadbod",
+    "kristijanhusak/vim-dadbod-completion",
+    "kristijanhusak/vim-dadbod-ui",
+  },
+  -- {
+  --   'rose-pine/neovim',
+  --   config = function()
+  --   require("rose-pine").setup({
+  --       variant = "auto", -- auto, main, moon, or dawn
+  --       dark_variant = "main", -- main, moon, or dawn
+  --       dim_inactive_windows = false,
+  --       extend_background_behind_borders = true,
+  --
+  --       enable = {
+  --           terminal = true,
+  --           legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+  --           migrations = true, -- Handle deprecated options automatically
+  --       },
+  --
+  --       styles = {
+  --           bold = true,
+  --           italic = true,
+  --           transparency = true,
+  --       },
+  --
+  --       groups = {
+  --           border = "muted",
+  --           link = "iris",
+  --           panel = "surface",
+  --
+  --           error = "love",
+  --           hint = "iris",
+  --           info = "foam",
+  --           note = "pine",
+  --           todo = "rose",
+  --           warn = "gold",
+  --
+  --           git_add = "foam",
+  --           git_change = "rose",
+  --           git_delete = "love",
+  --           git_dirty = "rose",
+  --           git_ignore = "muted",
+  --           git_merge = "iris",
+  --           git_rename = "pine",
+  --           git_stage = "iris",
+  --           git_text = "rose",
+  --           git_untracked = "subtle",
+  --
+  --           h1 = "iris",
+  --           h2 = "foam",
+  --           h3 = "rose",
+  --           h4 = "gold",
+  --           h5 = "pine",
+  --           h6 = "foam",
+  --       },
+  --
+  --       highlight_groups = {
+  --           -- Comment = { fg = "foam" },
+  --           -- VertSplit = { fg = "muted", bg = "muted" },
+  --       },
+  --
+  --       before_highlight = function(group, highlight, palette)
+  --           -- Disable all undercurls
+  --           -- if highlight.undercurl then
+  --           --     highlight.undercurl = false
+  --           -- end
+  --           --
+  --           -- Change palette colour
+  --           -- if highlight.fg == palette.pine then
+  --           --     highlight.fg = palette.foam
+  --           -- end
+  --     end,
+  --   })
+  --   vim.cmd("colorscheme rose-pine")
+  --   end
+  -- },
   {
     'rebelot/kanagawa.nvim',
     config = function()
@@ -71,21 +147,33 @@ require('lazy').setup({
       -- Default options:
     end
   },
+  --
+  -- {
+  --   'romgrk/barbar.nvim',
+  --   dependencies = {
+  --     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+  --     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+  --   },
+  --   init = function()
+  --     vim.g.barbar_auto_setup = true
+  --   end,
+  -- },
 
-  {
-    "sainnhe/everforest",
-    config = function()
-      -- vim.g.everforest_diagnostic_text_highlight=1
-      -- vim.g.everforest_diagnostic_line_highlight=1
-      -- vim.g.everforest_transparent_background=1
-      -- vim.g.everforest_diagnostic_virtual_text='highlighted'
-      -- vim.g.everforest_background='hard'
-      -- vim.g.everforest_ui_contrast='high'
-      -- vim.g.everforest_current_word='underline'
-      -- vim.cmd.colorscheme 'everforest'
-    end,
-  },
-
+  {"github/copilot.vim"},
+  -- {
+  --   "sainnhe/everforest",
+  --   config = function()
+  --     vim.g.everforest_diagnostic_text_highlight=1
+  --     vim.g.everforest_diagnostic_line_highlight=1
+  --     vim.g.everforest_transparent_background=1
+  --     vim.g.everforest_diagnostic_virtual_text='highlighted'
+  --     vim.g.everforest_background='hard'
+  --     vim.g.everforest_dim_inactive_windows=1
+  --     vim.g.everforest_ui_contrast='high'
+  --     vim.g.everforest_current_word='underline'
+  --     vim.cmd.colorscheme 'everforest'
+  --   end,
+  -- },
   {
     'nvim-tree/nvim-tree.lua',
     config = function()
@@ -160,6 +248,13 @@ require('lazy').setup({
     end
   },
 
+  {
+    "startup-nvim/startup.nvim",
+    requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+    config = function()
+      require "startup".setup()
+    end
+  },
   {
     'nvim-tree/nvim-web-devicons',
   },
@@ -330,6 +425,21 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_c = {
+          {
+            "filename",
+            path = 1
+          }
+        },
+        -- lualine_a = {},
+        -- lualine_c = {},
+        -- lualine_x = {},
+        -- lualine_y = {},
+        -- lualine_z = {}
+      },
+      tabline = {},
+      extensions = {}
     },
 
     -- sections = {
@@ -411,6 +521,7 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
+
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
@@ -453,7 +564,12 @@ vim.wo.wrap = true
 
 vim.wo.cursorline = true
 
+
 -- vim.wo.spell = true
+
+vim.opt.colorcolumn = "80"
+
+vim.o.laststatus = 3 -- global status
 
 
 -- [[ Basic Keymaps ]]
@@ -706,6 +822,7 @@ end
 --
 --  Add any additional override configuration in the following tables. They will be passed to
 --  the `settings` field of the server config. You must look up that documentation yourself.
+
 local servers = {
   clangd = {},
   gopls = {},
